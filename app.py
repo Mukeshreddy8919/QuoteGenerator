79c843d536d663aa74e36d168f6c292e57dd93e4
Mukesh reddy <mukheshreddycheerla@gmail.com>
2025-04-08 22:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -21,6 +22,7 @@ def get_quote():
     return jsonify({'reply': quote})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
 
